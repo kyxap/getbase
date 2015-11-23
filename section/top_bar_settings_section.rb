2275a877f0d93e4307965cbc37b1f9e979442be9
Roman <rkukharuk@gmail.com>
2015-11-23 17:11:39 +0200

    trying to fix the problem with el visibility

--- section/top_bar_settings_section.rb
+++ section/top_bar_settings_section.rb
@@ -15,7 +15,7 @@ class TopBarSettingSection
   end
 
   def open_settings_page
-    self.user_avatar_element.when_visible.click
+    self.user_avatar_element.click
     self.settings_element.when_visible.click
     return SettingsPage.new browser
   end
